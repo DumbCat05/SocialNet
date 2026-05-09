670f30bda40271c7c1547cd339c16275f4c9a519
DumbCat <dumbcat05@gmail.com>
2026-05-09 16:30:59 +0700

    Create simple PHP website

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First GitHub Website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Hello from GitHub</h1>
+    <p>This page was created on my PC, pushed to GitHub, then pulled on my Ubuntu VM.</p>
+
+    <?php
+        echo "<p>PHP is working on the VM.</p>";
+    ?>
+</body>
+</html>
