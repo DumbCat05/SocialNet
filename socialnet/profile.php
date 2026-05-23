ad0c9cca729fe281a018551b6dc8ebc0aa23e69e
DumbCat <dumbcat05@gmail.com>
2026-05-23 16:38:44 +0700

    profile

--- socialnet/profile.php
+++ socialnet/profile.php
@@ -47,7 +47,7 @@ $profileUser = $result->fetch_assoc();
             <h2>Profile Content</h2>
 
             <div class="profile-content">
-                <?php echo $description; ?>
+                <?php echo $profileUser["description"] ?? ""; ?>
             </div>
         <?php else: ?>
             <div class="message error">
